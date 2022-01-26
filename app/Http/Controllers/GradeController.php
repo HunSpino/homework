8a93fc3e6e8c0bc35a636ef3b1d7d7781d056ef5
HunSpino <tamasleventelaszlo99@gmail.com>
2022-01-26 12:04:44 +0100

    homecontroller és grade controller létrejött

---
+++ app/Http/Controllers/GradeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Grade;
+use Illuminate\Http\Request;
+
+class GradeController extends Controller
+{
+    public function index(){
+        $grades = Grade::orderBy('person')->get();
+        return view('grades.index', [ 'grades' => $grades]);
+    }
+}
